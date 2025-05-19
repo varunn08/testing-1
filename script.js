b160ae4feb45e5cf5def10c1f2d31f87443d0c46
varunn08 <149808232+varunn08@users.noreply.github.com>
2025-05-19 09:57:47 +0000

    array mathods

--- script.js
+++ script.js
@@ -7,4 +7,12 @@
 // let time = Date();
 
 // console.log(time.toString());
-console.log("hello guys i am back here !!");
+// console.log("hello guys i am back here !!");
+
+const a = ["hello", 'roshni', "love" ,];
+
+
+a.pop();
+a.push("i really loves you dear!!");
+
+console.log(a);
