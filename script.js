87f4fccd50a8a7498dfa8dc0bae63258c4674df0
varunn08 <149808232+varunn08@users.noreply.github.com>
2025-05-19 11:15:10 +0000

    code update

--- script.js
+++ script.js
@@ -9,10 +9,21 @@
 // console.log(time.toString());
 // console.log("hello guys i am back here !!");
 
-const a = ["hello", 'roshni', "love" ,];
+// const a = ["hello", 'roshni', "love" ,];
 
 
-a.pop();
-a.push("i really loves you dear!!");
+// a.pop();
+// a.push("i really loves you dear!!");
+// a.unshift("ishq");
+// a.shift();
+// a.shift();
+// a.shift();
+// a.shift();
+// a.unshift("fuck");
+// console.log(a);
 
+const x = ["rosh", "rb", "varun",["bhola", "roshni", "monu" ,["roshni bhola"]]];
+
+// const a = x.length;
+const a = x.flat(2);
 console.log(a);
